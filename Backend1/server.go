faad8c51b04e0f6d42a7dccd73bea11dc3e8804b
TanmayMahato <tanmaymahatodav@gmail.com>
2024-07-03 22:37:39 +0530

    initial commit

---
+++ Backend1/server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"html/template"
+
+	"log"
+	"net/http"
+)
+
+type Server struct {
+	ServerName string
+}
+
+func main() {
+
+	handler1 := func(w http.ResponseWriter, r *http.Request) {
+
+		tmpl := template.Must(template.ParseFiles("Frontend/index.html"))
+		//name := Server{ServerName: "server1"}
+
+		tmpl.Execute(w, nil)
+
+	}
+
+	http.HandleFunc("/", handler1)
+
+	log.Fatal(http.ListenAndServe(":8001", nil))
+}
